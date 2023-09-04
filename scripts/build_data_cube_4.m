00f0dd2876519066ee6298a0fb191ab660c26aa1
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2023-09-04 17:01:43 +0100

    (I think) Correct setup for range, doppler and SIMO processing

--- scripts/build_data_cube_4.m
+++ scripts/build_data_cube_4.m
@@ -3,16 +3,20 @@ function [dataCube] = build_data_cube_4(data, nFast, nRx, nTx, nSlow, k)
 %   Detailed explanation goes here
 
 nSlow = (nSlow / 2);
-% nCh = nRx * nTx;
+nCh = nRx * nTx;
 
 data1 = cmplx_from_array(data, k);
 
 
 % Reshape data into a 3D array with dimensions nchirp, nrb, and nch
-dataCube = reshape(data1, [nRx, nTx, nFast, nSlow]);
+% dataCube = reshape(data1, [nTx, nRx, nFast, nSlow]);
+dataCube = reshape(data1, [nRx, nFast, nTx, nSlow]);
 
 % [slow, fast, nRx, nTx]
-dataCube = permute(dataCube, [4, 3, 1, 2]);
+% dataCube = permute(dataCube, [4, 3, 2, 1]);
+dataCube = permute(dataCube, [4, 2, 1, 3]);
+dataCube = squeeze(dataCube(:, :, :, 1));
+dataCube = permute(dataCube, [2, 3, 1]);
 
 
 end
--- scripts/cmplx_from_array.m
+++ scripts/cmplx_from_array.m
@@ -4,20 +4,31 @@ function [cmplxArray] = cmplx_from_array(array,k)
 if mod(length(array), k) ~= 0
     error("Length of array is not divisible by k")
 end 
+% 
+% oItLen = length(array)/(2*k);
+% cmplxArray = [];
+% 
+% for oIt=1:oItLen
+% 
+%     % disp(array(oIt*2*k));
+%     indx = (oIt-1)*2*k;
+% 
+%     for iIt=1:k
+%         real = array(indx + iIt);
+%         imag = array(indx + iIt + k);
+%         cmplxArray(end + 1) = complex(real, imag);
+%     end 
+% end 
 
-oItLen = length(array)/(2*k);
-cmplxArray = [];
+oItLen = length(array) / (2 * k);
+cmplxArray = complex(zeros(1, oItLen * k));
 
-for oIt=1:oItLen
+for oIt = 1:oItLen
+    indx = (oIt - 1) * 2 * k;
+    realValues = array(indx + 1 : indx + k);
+    imagValues = array(indx + k + 1 : indx + 2 * k);
+    cmplxArray((oIt - 1) * k + 1 : oIt * k) = complex(realValues, imagValues);
+end
 
-    % disp(array(oIt*2*k));
-    indx = (oIt-1)*2*k;
-
-    for iIt=1:k
-        real = array(indx + iIt);
-        imag = array(indx + iIt + k);
-        cmplxArray(end + 1) = complex(real, imag);
-    end 
-end 
 end
 
